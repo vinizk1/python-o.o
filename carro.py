e060d55f793a0f64003bdb595fff03737433f4c8
vinizk1 <128698338+vinizk1@users.noreply.github.com>
2024-04-16 09:20:25 -0300

    Add files via upload

---
+++ carro.py
@@ -0,0 +1,20 @@
+class Carro:
+    def __init__(self, marca, modelo, cor, ano):
+        self.marca = marca
+        self.modelo = modelo
+        self.cor = cor
+        self.ano = ano
+
+def exibInfo(self):
+    print("marca: ", self.marca)
+    print("modelo: ", self.modelo)
+    print("cor: ", self.cor)
+    print("ano: ", self.ano)
+
+
+carro1 = Carro('Fiat', 'Uno', 'Preto', 2010)
+carro2 = Carro('Mustang', 'Shelby', 'Vermelho', 2022)
+
+
+input()
+
